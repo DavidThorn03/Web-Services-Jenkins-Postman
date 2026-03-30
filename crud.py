e39b137c35d09efcae6a245a658e42ebd7a1a1bc
DavidThorn03 <b00152842@mytudublin.ie>
2026-03-30 17:36:00 +0100

    PDF

--- crud.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-def insert(data, collection, many=False):
-    if many:
-        return collection.insert_many(data)
-    return collection.insert_one(data)
-
-def get(collection, value, identifier="_id"):
-    return collection.find_one({identifier: value})
